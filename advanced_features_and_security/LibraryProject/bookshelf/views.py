9ea32a08a96b691876d26627be7bb09da6272119
Rhoda <rhodaugboro@gmail.com>
2025-11-17 20:59:06 +0100

    Managing permissions and groups

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,41 +1,41 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Book
-from .forms import BookForm  
+from .forms import BookForm
 
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
-def list_books(request):
+def book_list(request): 
     books = Book.objects.all()
     return render(request, 'bookshelf/list_books.html', {'books': books})
 
 @login_required
 @permission_required('bookshelf.can_create', raise_exception=True)
-def create_book(request):
+def book_create(request):  
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('list_books')
+            return redirect('book_list') 
     else:
         form = BookForm()
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
 @login_required
 @permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_book(request, pk):
+def book_edit(request, pk):
     book = get_object_or_404(Book, pk=pk)
     form = BookForm(request.POST or None, instance=book)
     if form.is_valid():
         form.save()
-        return redirect('list_books')
+        return redirect('book_list')
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
 @login_required
 @permission_required('bookshelf.can_delete', raise_exception=True)
-def delete_book(request, pk):
+def book_delete(request, pk):
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         book.delete()
-        return redirect('list_books')
+        return redirect('book_list')
     return render(request, 'bookshelf/confirm_delete.html', {'object': book})
