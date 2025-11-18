b098b3f23626220b312aa3a957294ccde2fea6eb
Rhoda <rhodaugboro@gmail.com>
2025-11-18 16:52:54 +0100

    Implementing security best practices

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.db.models import Q
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm, ExampleForm 
 
 # -------------------------------
 # SECURE BOOK LIST VIEW
