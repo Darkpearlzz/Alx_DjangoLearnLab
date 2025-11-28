d0a5eba1536e19dfac85dd3dbcc764fc32818b84
Rhoda <rhodaugboro@gmail.com>
2025-11-28 20:39:57 +0100

    Implementing Filtering, Searching a nd Ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import generics, mixins, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 from django_filters.rest_framework import DjangoFilterBackend
-
+from django_filters import rest_framework  
 class AuthorViewSet(viewsets.ModelViewSet):
     """
     CRUD endpoints for Author.
