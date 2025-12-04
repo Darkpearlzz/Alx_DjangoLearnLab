b711fe217c1d8d0a5e663c4ea81e87a41316b5c4
Rhoda <rhodaugboro@gmail.com>
2025-12-04 22:19:36 +0100

    Feat: Blog Post Management

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
 
     # ----- Post CRUD views -----
     path("posts/", views.PostListView.as_view(), name="post_list"),
-    path("posts/new/", views.PostCreateView.as_view(), name="post_create"),
-    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_update"),
-    path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
+    path("post/new/", views.PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
 
     # ----- Authentication views -----
     path("login/", views.CustomLoginView.as_view(), name="login"),
