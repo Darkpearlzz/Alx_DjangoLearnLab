6897a29a449698728de939d026e09704859c2cfb
Rhoda <rhodaugboro@gmail.com>
2025-12-04 22:16:35 +0100

    Feat: Blog Post Management

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
 
+    # ----- Post CRUD views -----
+    path("posts/", views.PostListView.as_view(), name="post_list"),
+    path("posts/new/", views.PostCreateView.as_view(), name="post_create"),
+    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_update"),
+    path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
+
     # ----- Authentication views -----
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", views.CustomLogoutView.as_view(), name="logout"),
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -3,8 +3,11 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from .forms import RegisterForm  
+from .forms import RegisterForm, PostForm  # Make sure PostForm exists
 
 # ----- Blog views -----
 def index(request):
@@ -22,7 +25,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             messages.success(request, "Account created successfully!")
-            return redirect("login")
+            return redirect("blog:login")
     else:
         form = RegisterForm()
     return render(request, "blog/register.html", {"form": form})
@@ -42,5 +45,45 @@ def profile_view(request):
         request.user.email = request.POST.get("email")
         request.user.save()
         messages.success(request, "Profile updated!")
-        return redirect("profile")
+        return redirect("blog:profile")
     return render(request, "blog/profile.html")
+
+
+# ----- Post CRUD views -----
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
+    paginate_by = 5  # optional
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("blog:post_list")
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
